89d05d9e3c44bc8707d12397a448fc0e68cd4c44
Innocent Goodness <innocentgoodness009@gmail.com>
2025-04-01 17:19:08 +0100

    Api Key fixed

--- components/utils/supabaseClient.ts
+++ components/utils/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("Supabase URL or Anon Key is not defined in environment variables.");
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
